d160a8eb7ebda2610e6ea3bf2e90682199e57159
PrinceYadav-001 <princeyadav6759@gmail.com>
2026-06-14 19:25:45 +0530

    Add dynamic image showing choices

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 //setup
 let humanScore = 0;
 let computerScore = 0;
+
+const ChoiceMap = {
+    0 : "rock",
+    1 : "paper",
+    2 : "scissors"
+}
+
 const rockBtn = document.querySelector("#rock-btn");
 const paperBtn = document.querySelector("#paper-btn");
 const scissBtn = document.querySelector("#scissors-btn");
 const outcomeTxt = document.querySelector("#outcome-txt");
 const yourScoreTxt = document.querySelector("#your-score");
+const compScoreTxt = document.querySelector("#comp-score");
+const playerDisplay = document.querySelector(".player-display");
+const computerDisplay = document.querySelector(".computer-display");
 
 //add event Listeners
-rockBtn.addEventListener("click", () => playRound("rock"));
-paperBtn.addEventListener("click", () => playRound("paper"));
-scissBtn.addEventListener("click", () => playRound("scissors"));
+rockBtn.addEventListener("click", () => playRound(0));
+paperBtn.addEventListener("click", () => playRound(1));
+scissBtn.addEventListener("click", () => playRound(2));
 
 
 //main
@@ -20,61 +30,56 @@ scissBtn.addEventListener("click", () => playRound("scissors"));
 
 function playRound(humanChoice)
 {
-    let computerChoice = "rock";
+    let computerChoice = getComputerChoice();
+    updateImage(humanChoice, computerChoice);
+    updateScore(humanChoice, computerChoice);
+    checkEndgame();
+}
 
+function getComputerChoice()
+{
     // pick a random number from 0, 1, 2
-    //   i.e. floor(random * 3)
     let randomNum = Math.floor(Math.random() * 3);
+    return randomNum;
+}
 
-    //  if num = 0 toh "rock" ,
-    //  if num = 1 toh "paper",
-    //  if num = 2 toh "scissors" 
-    switch(randomNum){
-        case 0:
-            computerChoice = "rock";
-        case 1:
-            computerChoice = "paper";
-        case 2:
-            computerChoice = "scissors";
-    }
-    
 
-    let choiceMap = {
-        "rock" : 0,
-        "paper" : 1,
-        "scissors" : 2,
-    }
-
-    humanChoice = choiceMap[humanChoice];
-    computerChoice = choiceMap[computerChoice];
-
+function updateImage(humanChoice, computerChoice)
+{
+    playerDisplay.childNodes[0].src = `images/${ChoiceMap[humanChoice]}.svg`;
+    computerDisplay.childNodes[0].src = `images/${ChoiceMap[computerChoice]}.svg`;
+}
+//update Score according to Outcome
+function updateScore(humanChoice, computerChoice)
+{
     // humanChoice - computerChoice =
     // 0 toh draw
-    // 1 toh win
-    // -1 toh lose
-    // 2 toh lose
-    // -2 toh wingetComputerChoice()
-
+    // 1 ya -2 toh win
+    // 2 ya -1 toh lose
     switch(humanChoice - computerChoice)
     {
         case 0:
             break;
 
-        case -1:
-        case 2:
-            computerScore++;
-            break;
-
-        case -2:
         case 1:
+        case -2:
             humanScore++;
             break;
+        
+        case 2:
+        case -1:
+            computerScore++;
+            break;
     }
-
-    yourScoreTxt.textContent = humanScore;
-    checkEndgame();
+    
+    updateScoreText();
 }
 
+function updateScoreText()
+{
+    yourScoreTxt.textContent = humanScore;
+    compScoreTxt.textContent = computerScore;
+}
 
 function checkEndgame()
 {
@@ -82,7 +87,7 @@ function checkEndgame()
     {
         outcomeTxt.textContent = "You Win";
     }
-    else if (humanScore == 5)
+    else if (computerScore == 5)
     {
         outcomeTxt.textContent = "You lose";
     }
